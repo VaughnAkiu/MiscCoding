c3bc1a58c2ccb6a153a890916f3446a0d9e1a547
Vaughn Akiu <vkakiu@gmail.com>
2021-02-19 13:32:27 -0500

    progression

--- Java/LeetCode/src/ReverseInteger.java
+++ Java/LeetCode/src/ReverseInteger.java
@@ -13,7 +13,12 @@ public class ReverseInteger
     {
         //convert int to string
         String s = String.valueOf(input);
-
+        //store string in char array
+        char[] c = new char[s.length()];
+        for(int i = 0; i < s.length(); i++)
+        {
+            c[i] = s.charAt(i);
+        }
 
         return output;
     }
