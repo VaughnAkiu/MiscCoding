c2c5220f2c5b7cb4348ec76b4b0731fb6327eeda
Vaughn Akiu <vkakiu@gmail.com>
2021-02-19 12:20:14 -0500

    progression RomanToInteger, accounting for special cases

--- Java/LeetCode/src/RomanToInteger.java
+++ Java/LeetCode/src/RomanToInteger.java
@@ -1,7 +1,7 @@
 public class RomanToInteger {
 
 
-        static String s = "III";
+        static String s = "XIV";
         static int romanValue = 0; //placeholder to hold roman numeral value
         static int result;
 
@@ -17,12 +17,38 @@ public class RomanToInteger {
                 {
                     romanValue = romanValue + 5;
                 }
+                else if(s.charAt(i) == 'X')
+                {
+                    romanValue = romanValue + 10;
+                }
             }
-
             result = romanValue;
         }
 
-        
+        //check special cases
+        //such as IV, IX, XL, etc...
+        //check next character as well, if combination of these values then adjust to corresponding value
+        public static void specialCase(String s)
+        {
+            for(int i = 0; i < s.length(); i++)
+            {
+                if(s.charAt(i) == 'I')
+                {
+                    //check for special cases
+                    if(i+1 < s.length() & s.charAt(i+1) == 'V') {
+                        romanValue = romanValue + 4;
+                        i++; //increment past the next character since we are accounting for it here
+                    }
+                    else if (i+1 < s.length() & s.charAt(i+1) == 'X')
+                    {
+                        romanValue = romanValue + 9;
+                        i++;
+                    }
+                    else{romanValue++;} //there is no special case
+                }
+            }
+        }
+
 
         public static void main(String[] args) {
             convertString(s);
