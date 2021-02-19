db3017085e6c5ef93b4a925c3783e24ea54596a0
Vaughn Akiu <vkakiu@gmail.com>
2021-02-19 16:29:03 -0500

    coding the basis in for, BinaryTreeLevelOrderTraversal

--- Java/LeetCode/src/BinaryTreeLevelOrderTraversal.java
+++ Java/LeetCode/src/BinaryTreeLevelOrderTraversal.java
@@ -1,9 +1,35 @@
 public class BinaryTreeLevelOrderTraversal
 {
+    static int node[] = {1, 2, 3, 4, 5, 6};
 
+    public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+      //input is formatted root = [1,2,3,null,null,6,7,etc]
+    //output should be formatted [[1],[2,3],[6,7]]
+      //first value is always root
+
+    //print array
+    public static void printNode(int[] node)
+    {
+        for(int i = 0; i < node.length; i++)
+        {
+            System.out.println(node[i]);
+        }
+    }
 
     public static void main(String[] args)
     {
-        System.out.println("hello");
+        printNode(node);
     }
 }
