86bc934577cc1aaa78a1b033fc0610737ad46f59
Vaughn Akiu <vkakiu@gmail.com>
2021-02-19 13:24:31 -0500

    brainstorming possible solution avenues

--- Java/LeetCode/src/ReverseInteger.java
+++ Java/LeetCode/src/ReverseInteger.java
@@ -1,9 +1,31 @@
 public class ReverseInteger
 {
+    static int input = 123;
+    static int output = 0;
+
+    //solution 1
+    //convert int to string
+    //store string into char array
+    //reverse order of char array
+    //convert char array into string
+    //convert string into int
+    public static int reverseInt(int input)
+    {
+        //convert int to string
+        String s = String.valueOf(input);
 
 
+        return output;
+    }
 
+    //solution 2
+    //check value of int
+    //if value is >9 then use a two char array
+    //if value is > 99 then use 3 char array etc.
+    //store each ones, tens, hundreds etc value in corresponding array
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        reverseInt(input);
+        System.out.println(output);
     }
 }
