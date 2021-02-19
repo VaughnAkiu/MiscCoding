4cab964721b0320e765be8799bd8476df9bfefe5
Vaughn Akiu <vkakiu@gmail.com>
2021-02-19 11:50:14 -0500

    progression on RomanToInteger

--- Java/LeetCode/src/RomanToInteger.java
+++ Java/LeetCode/src/RomanToInteger.java
@@ -2,29 +2,44 @@ public class RomanToInteger {
 
 
         String s = "III";
+        int romanValue = 0; //placeholder to hold roman numeral value
 
-        char[] charArray = new char[s.length];
+        //char[] charArray = new char[s.length()];
 
         public int romanToInt(String s) {
             return 1;
         }
-
+        /*
         //read given string into a character array
         public void stringToArray(String s){
 
-            for(int i = 0; i < s.length; i++)
+            for(int i = 0; i < s.length(); i++)
             {
-                charArray[i] = str.charAt(i);
+                charArray[i] = s.charAt(i);
             }
-        }
+        }*/
 
         //convert string to numerals
         public void convertString(String s){
-
+            for(int i = 0; i < s.length(); i++)
+            {
+                if(s.charAt(i) == 'I')
+                {
+                    romanValue++;
+                }
+                else if(s.charAt(i) == 'V')
+                {
+                    romanValue = romanValue + 5;
+                }
+            }
         }
+
+
+
 
         public static void main(String[] args) {
             System.out.println("hello");
+            System.out.println("hello".charAt(1));
         }
 
 
