57cc8c4739106f6c1caf6f40d1148311990d0362
Vaughn Akiu <vkakiu@gmail.com>
2021-02-19 11:21:56 -0500

    setting up LeetCode directory in IntelliJ

---
+++ Java/LeetCode/src/RomanToInteger.java
@@ -0,0 +1,32 @@
+public class RomanToInteger {
+
+
+        String s = "III";
+
+        char[] charArray = new char[s.length];
+
+        public int romanToInt(String s) {
+            return 1;
+        }
+
+        //read given string into a character array
+        public void stringToArray(String s){
+
+            for(int i = 0; i < s.length; i++)
+            {
+                charArray[i] = str.charAt(i);
+            }
+        }
+
+        //convert string to numerals
+        public void convertString(String s){
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println("hello");
+        }
+
+
+
+}
