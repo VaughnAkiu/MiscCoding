4dcd7a3845863ede0a524d44763f8df5c1295d93
Vaughn Akiu <vkakiu@gmail.com>
2021-04-15 11:13:22 -0400

    assign sql query results to pl / sql variables

--- SQL/Test.sql
+++ SQL/Test.sql
@@ -184,5 +184,18 @@ INSERT INTO members(id, first_name, last_name, movie_id) VALUES(1, Adam, Smith, 
 		dbms_output.put_line(message);
 	END;
   
+  --assign sql query results to pl/sql variables
+  CREATE TABLE CUSTOMERS(
+	ID	INT NOT NULL,
+	NAME VARCHAR (20) NOT NULL,
+	AGE INT NOT NULL,
+	ADDRESS CHAR (25),
+	SALARY DECIMAL (18,2),
+	PRIMARY KEY(ID)
+  );
+  INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+  VALUES(1,'Lors',32,'Darush',1920.00);
+  
+
   
   
